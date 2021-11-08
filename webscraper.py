72721e04b1aea0a046088fef31436b9a4e0f02a8
Man <mhnguyen@hamilton.edu>
2021-11-07 23:51:19 -0500

    added some stuff

--- webscraper.py
+++ webscraper.py
@@ -1,2 +1,13 @@
 from selenium import webdriver
 import pandas as pd
+import numpy as np
+
+def click_events():
+    return 0
+
+def main():
+    url = "https://play.usaultimate.org/events/D-III-College-Championships-2019/schedule/Men/CollegeMen/"
+    return 0
+
+if __name__ == "__main__":
+    main()
