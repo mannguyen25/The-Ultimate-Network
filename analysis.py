32d47ce983db155742bdf0e1833dfdc731190bf1
nradair <63612341+nradair@users.noreply.github.com>
2021-11-14 23:58:57 -0500

    Bipartite graph added

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
-"""
+'''
 Creates edge lists for a directed weighted 2019 graph and for a bipartite graph
-"""
+of tournaments and participating teams.
+'''
 
 import networkx as nx
 from networkx.algorithms import bipartite
@@ -12,7 +13,7 @@ import os
 import re
 
 
-def make_edge_list(csv_list):
+def make_team_graph(csv_list):
     graph = nx.DiGraph()
 
     for csv in csv_list:
@@ -44,14 +45,18 @@ def make_edge_list(csv_list):
             graph.add_edge(df[column_names[2]][i], df[column_names[1]][i], weight=weight2)
 
     edge_list = nx.to_pandas_edgelist(graph)
-    edge_list.to_csv(path_or_buf="sanctioned_edgelist.csv", index=False)
+    edge_list.to_csv(path_or_buf='sanctioned_edgelist.csv', index=False)
+    return graph
 
 
-def bipartite_graph(csv_list):
-    graph = nx.Graph()
+def make_bipartite_graph(csv_list):
+    bigraph = nx.Graph()
+    edge_ids = pd.DataFrame()
 
     for csv in csv_list:
         filename = csv[2:-4]
+        edge_ids = edge_ids.append({'id': filename, 'category': 'tournament'}, ignore_index=True)
+
         df = pd.read_csv(csv)
 
         column_names = []
@@ -67,43 +72,42 @@ def bipartite_graph(csv_list):
             if away not in teams:
                 teams.append(away)
 
-        graph.add_node(filename, bipartite=0)
-        graph.add_nodes_from(teams, bipartite=1)
+        bigraph.add_node(filename, bipartite=0)
+        bigraph.add_nodes_from(teams, bipartite=1)
         for team in teams:
-            graph.add_edge(filename, team)
+            bigraph.add_edge(filename, team)
+            edge_ids = edge_ids.append({'id': team, 'category': 'team'}, ignore_index=True)
+
+    edge_ids.to_csv(path_or_buf='bipartite_labels.csv', index=False)
+    edge_list = nx.to_pandas_edgelist(bigraph)
+    edge_list.to_csv(path_or_buf='bipartite_edgelist.csv', index=False)
+
+    # Create student and class projections
+    tournaments = {n for n, d in bigraph.nodes(data=True) if d['bipartite'] == 0}
+    teams = set(bigraph) - tournaments
+    team_graph = bipartite.projected_graph(bigraph, teams)
+    tournament_graph = bipartite.projected_graph(bigraph, tournaments)
+
+    return bigraph, team_graph, tournament_graph
 
 
-# Create student and class projections
-# student_nodes = {n for n, d in bipartite_graph.nodes(data=True) if d["bipartite"] == 0}
-# class_nodes = set(bipartite_graph) - student_nodes
-#
-# student_graph = bipartite.projected_graph(bipartite_graph, student_nodes)
-#
-# class_graph = bipartite.projected_graph(bipartite_graph, class_nodes)
+def analyze(graph, file_object=None):
+    # Degree assortativity
+    assor = nx.algorithms.assortativity.degree_assortativity_coefficient(graph)
+    print(f'\tThe degree assortativity is {assor}', file=file_object)
 
-
-# Degree assortativity
-# print(nx.algorithms.assortativity.degree_assortativity_coefficient(nationals_graph))
-
-
-# Redundancy
-# redundancies = bipartite.node_redundancy(student_graph)
-# for name in redundancies:
-#     print(f"{name} has redundancy {redundancies[name]}\\\\")
-
-
-# k-components
-# k_components = nx.algorithms.connectivity.k_components(nationals_graph)
-# for num in k_components:
-#     if num > 5:
-#         print(f"The {num}-components are {k_components[num]}\\\\")
+    # k-components
+    # k_components = nx.algorithms.connectivity.k_components(nationals_graph)
+    # for num in k_components:
+    #     if num > 5:
+    #         print(f'The {num}-components are {k_components[num]}\\\\')
 
 
 # Histograms
 # degree_sequence = sorted([d for n, d in class_graph.degree()])
 # bin_size = list(n for n in range(1, 150))
 # plt.hist(degree_sequence, bins=bin_size)
-# plt.savefig("Class Histogram2.png")
+# plt.savefig('Class Histogram2.png')
 
 # p_k = list(0 for _ in range(degree_sequence[-1]))
 # for i in range(degree_sequence[-1]):
@@ -120,21 +124,39 @@ def bipartite_graph(csv_list):
 # plt.savefig('Class loglog')
 
 
+def bianalyze(graph, file_object=None):
+    # Redundancy
+    redundancies = bipartite.node_redundancy(graph)
+    print(f'\tThe average redundancy is {sum(redundancies)/len(redundancies)}', file=file_object)
+    for name in redundancies:
+        print(f'\t{name} has redundancy {redundancies[name]}', file=file_object)
+
+
 def main():
     sanctioned = './Sanctioned 2019'
     nonsanctioned = './Non-Sanctioned 2019'
     csv_list = ['./D-I College Championships.csv', './nationals.csv']
     # csv_list = ['./sanctioned2019.csv', './nationals.csv']
     for entry in os.scandir(sanctioned):
-        if entry.path.endswith(".csv"):
+        if entry.path.endswith('.csv'):
             csv_list.append(entry.path)
     for entry in os.scandir(nonsanctioned):
-        if entry.path.endswith(".csv"):
+        if entry.path.endswith('.csv'):
             csv_list.append(entry.path)
 
-    make_edge_list(csv_list)
-    # make_bipartite_edgelist(csv_list)
+    games = make_team_graph(csv_list)
+    part, tour, team = make_bipartite_graph(csv_list)
+
+    label = ['games', 'bipartite', 'tournaments', 'teams']
+    graphs = [games, part, tour, team]
+
+    with open('Analysis Results.txt', 'w') as file_object:
+        for index, graph in enumerate(graphs):
+            print(f'For the {label[index]} graph:', file=file_object)
+            analyze(graph, file_object)
+        # print('For the bipartite graph:', file=file_object)
+        # bianalyze(part, file_object)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
