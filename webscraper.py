2ee9fe81bd7ef66f475aed545c2b0ab490ca984c
mannguyen25 <mhnguyen@hamilton.edu>
2021-12-01 08:27:20 -0500

    fixed regex

--- webscraper.py
+++ webscraper.py
@@ -25,28 +25,35 @@ def scrape_page(page):
     # gets each pool eg. Pool A, B, C, D
     pools = soup.findAll("table", {"class":"global_table scores_table"})
     games = []
+    key = {"W": 15, "F": 0, "L":0}
     for pool in pools:
         entries = pool.find_all("span", {"class":"adjust-data"})
         for i in range(0,len(entries),8):
-            date = entries[i].string
+            # date = entries[i].string
             if entries[i+3].a is None or entries[i+4].a is None:
                 continue
-            home = entries[i+3].a.string
-            home = re.search("[a-zA-Z]+(\s[a-zA-Z]+)?", home)
+            home = re.sub("[\(\)]", "", entries[i+3].a.string)
+            home = re.search("[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*", home)
             if home is not None:
                 home = home.group()
             else:
                 continue                        
-            away = entries[i+4].a.string
-            away = re.search("[a-zA-Z]+(\s[a-zA-Z]+)?", away)
+            away = re.sub("[\(\)]", "",entries[i+4].a.string)
+            away = re.search("[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*", away)
             if away is not None:
                 away = away.group()  
             else:
                 continue
-            h_score = entries[i+5].string
-            a_score = entries[i+6].string
-            game = [date,home,away,h_score,a_score]
-            games.append(game)
+            h_score = key[entries[i+5].string] if entries[i+5].string in key else int(entries[i+5].string)
+            a_score = key[entries[i+6].string] if entries[i+6].string in key else int(entries[i+6].string)
+            team = [home, away]
+            winner = team.pop(team.index(home)) if h_score > a_score else team.pop(team.index(away))
+            loser = team.pop(0)
+            if abs(h_score-a_score) == 0:
+                continue
+            else:
+                game = [winner, loser, abs(h_score-a_score)]
+                games.append(game)
     teams = soup.findAll("span", {"class":"team adjust-data"})
     scores = soup.findAll("span", {"class":"score adjust-data"})
     for i in range(0, len(teams), 2):
@@ -55,38 +62,45 @@ def scrape_page(page):
         date = date.split(" ")[0]
         if teams[i].a is None or teams[i+1].a is None:
             continue
-        home = teams[i].a.string
-        home = re.search("[a-zA-Z]+(\s[a-zA-Z]+)?", home)
+        home = re.sub("[\(\)]", "",teams[i].a.string)
+        home = re.search("[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*", home)
         if home is not None:
             home = home.group()
         else:
             continue    
-        away = teams[i+1].a.string
-        away = re.search("[a-zA-Z]+(\s[a-zA-Z]+)?", away)
+        away = re.sub("[\(\)]", "",teams[i+1].a.string)
+        away = re.search("[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*", away)
         if away is not None:
             away = away.group()  
         else:
             continue
-        h_score = scores[i].string
-        a_score = scores[i+1].string
-        game = [date, home,away,h_score,a_score]
-        games.append(game)
+        h_score = key[scores[i].string] if scores[i].string in key else int(scores[i].string)
+        a_score = key[scores[i+1].string] if scores[i+1].string in key else int(scores[i+1].string)
+        team = [home, away]
+        winner = team.pop(team.index(home)) if h_score > a_score else team.pop(team.index(away))
+        loser = team.pop(0)
+        if abs(h_score-a_score) == 0:
+            continue
+        else:
+            game = [winner, loser, abs(h_score-a_score)]        
+            games.append(game)
     return games
 def main():
     # TODO: implement selenium driver to select multiple pages :(
     # starting url
-    url = "https://play.usaultimate.org/events/Layout-Pigout-2019/schedule/Men/CollegeMen/"
+    url = "https://play.usaultimate.org/events/D-III-College-Championships-2019/schedule/Men/CollegeMen/"
     page = requests.get(url)
     data = scrape_page(page)
-    # header = ["Date", "Home Team", "Away Team", "Home Score", "Away Score"]
-    write_to_csv(data, "C:/Users/Man/Documents\GitHub/The-Ultimate-Network/Non-Sanctioned 2019/Layout Pigout 2019.csv")
+    # # header = ["Date", "Home Team", "Away Team", "Home Score", "Away Score"]
+    write_to_csv(data, "C:/Users/Man/Documents\GitHub/The-Ultimate-Network/D-III College Championships.csv")
     # url = "https://archive.usaultimate.org/archives/2019_college.aspx#regionals"
-    # # initialize latest driver
+    # initialize latest driver
     # page = requests.get(url)
     # soup = bs.BeautifulSoup(page.text,'html.parser')
     # for a in soup.find_all('a', href=re.compile('(https:\/\/play.usaultimate.org\/events\/).+(Mens).+')):
     #     data = scrape_page(requests.get(a['href']+"/schedule/Men/CollegeMen/"))
-    #     # header = ["Date", "Home Team", "Away Team", "Home Score", "Away Score"]
-    #     write_to_csv(data, "C:/Users/Man/Documents/GitHub/The-Ultimate-Network/Events"+a.string + ".csv")
+        # header = ["Date", "Home Team", "Away Team", "Home Score", "Away Score"]
+        # write_to_csv(data, "C:/Users/Man/Documents/GitHub/The-Ultimate-Network/games(3).csv")
+        # write_to_csv(data, "C:/Users/Man/Documents/GitHub/The-Ultimate-Network/Sanctioned 2019/"+a.string + ".csv")
 if __name__ == "__main__":
     main() 
